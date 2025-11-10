7606e8a5e3451e6ee01b02e502e909592e07a6b5
Your GitHub username <your@email.com>
2025-11-10 23:40:01 +0100

    Fix duplicate RootLayout and apply Gontserrat font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,25 @@
-import './globals.css';
-import Navbar from './components/Navbar/index';
-import Footer from './components/Footer/Footer';
-
-import { gontserrat } from './fonts';
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body className={gontserrat.className}>{children}</body>
-    </html>
-  );
-}
+import "./globals.css";
+import Navbar from "./components/Navbar/index";
+import Footer from "./components/Footer/Footer";
+import { gontserrat } from "./fonts";
 
 export const metadata = {
-  title: 'Designs4suc6',
-  description: 'Designs4suc6 - Wordpress Web Design',
-}
+  title: "Designs4suc6",
+  description: "Designs4suc6 - Wordpress Web Design",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className={gontserrat.className}>
         {/* <Navbar /> */}
         {children}
         {/* <Footer /> */}
       </body>
     </html>
-  )
+  );
 }
